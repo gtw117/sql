e93f9a47e070471bb883d07559cf4c257355f271
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-04-05 11:59:09 -0500

    Update

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,5 +8,23 @@ select * from employees where last_name like '%q%';
 
 -- where continued 8.7.2
 
-select * from employees where emp_no < 10200;
-select * from employees where emp_no < 10200 and gender = 'f';
+select * from employees 
+	where emp_no < 10200;
+select * from employees 
+	where emp_no < 10200 and gender = 'f';
+select * from employees 
+	where first_name = 'irena' or first_name = 'vidya' or first_name = 'maya';
+select * from employees 
+	where (first_name = 'irena' or first_name = 'vidya' or first_name = 'maya') and gender = 'm';
+select * from employees 
+	where last_name like 'e%' or last_name like '%e';
+select * from employees 
+	where last_name like 'e%' and last_name like '%e';
+select * from employees
+	where birth_date like '%12-25';
+select * from employees
+	where hire_date like '199%';
+select * from employees
+	where hire_date like '199%' and birth_date like '%12-25';
+select * from employees
+	where last_name like ''

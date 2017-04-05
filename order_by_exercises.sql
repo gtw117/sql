01946cbf0c7035d71416532bc9a9ebde75c7533f
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-04-05 13:52:14 -0500

    Update

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,12 +1,11 @@
--- where exercise 8.7.1
-select * from employees where first_name in ('irena', 'vidya', 'maya');
+-- order by 8.7.3
+
+select * from employees where first_name in ('irena', 'vidya', 'maya') order by first_name;
 select last_name from employees where last_name like 'e%';
 select * from employees where hire_date between '1990-01-01' and '1999-12-31';
 select * from employees where birth_date like '%12-25';
 select * from employees where last_name like '%q%';
 
-
--- where continued 8.7.2
 
 select * from employees 
 	where emp_no < 10200;
@@ -15,17 +14,14 @@ select * from employees
 select * from employees 
 	where first_name = 'irena' or first_name = 'vidya' or first_name = 'maya';
 select * from employees 
-	where (first_name = 'irena' or first_name = 'vidya' or first_name = 'maya') and gender = 'm';
+	where (first_name = 'irena' or first_name = 'vidya' or first_name = 'maya') and gender = 'm' 
+	order by first_name, last_name;
+
 select * from employees 
-	where last_name like 'e%' or last_name like '%e';
-select * from employees 
-	where last_name like 'e%' and last_name like '%e';
+	where last_name like 'e%' order by emp_no;
+
 select * from employees
-	where birth_date like '%12-25';
-select * from employees
-	where hire_date like '199%';
-select * from employees
-	where hire_date like '199%' and birth_date like '%12-25';
+	where hire_date like '199%' and birth_date like '%12-25' order by birth_date asc, hire_date desc;
 
 
 select first_name, last_name
@@ -39,3 +35,4 @@ select * from employees
 
 select * from employees
 	where first_name = 'leni' order by gender desc, last_name desc;
+
